98f144650ab56d2ebe915e406c6e1819510a198d
seungma93 <seungma93@naver.com>
2025-11-29 21:31:29 +0900

    Chore: 레트로핏 클라이언트 생성

---
+++ app/src/main/java/com/seungma/daglo/network/RetrofitClient.kt
@@ -0,0 +1,109 @@
+package com.seungma.daglo.network
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okio.Buffer
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.TimeUnit
+
+
+object RetrofitClient {
+    private const val BASE_URL = "https://rickandmortyapi.com/api"
+    private const val RETROFIT_TIMEOUT_NEW = 15.toLong()
+
+    private val interceptorClient = OkHttpClient().newBuilder()
+        .addInterceptor(RequestInterceptor())
+        .addInterceptor(ResponseInterceptor())
+        .addInterceptor(CurlLoggingInterceptor())
+        .connectTimeout(RETROFIT_TIMEOUT_NEW, TimeUnit.SECONDS)
+        .readTimeout(RETROFIT_TIMEOUT_NEW, TimeUnit.SECONDS)
+        .writeTimeout(RETROFIT_TIMEOUT_NEW, TimeUnit.SECONDS).build()
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(interceptorClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+}
+
+class RequestInterceptor() : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder = chain.request().newBuilder()
+
+        return chain.proceed(builder.build())
+    }
+}
+
+class ResponseInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val response = chain.proceed(request)
+
+        when (response.code) {
+            200 -> {
+
+            }
+
+            400 -> {
+
+            }
+
+            401 -> {
+
+            }
+
+            402 -> {
+
+            }
+
+            403 -> {
+
+            }
+            500 -> {
+
+            }
+            else -> {
+
+            }
+        }
+        return response
+    }
+}
+
+class CurlLoggingInterceptor : Interceptor {
+
+    companion object {
+        private val UTF8 = StandardCharsets.UTF_8
+    }
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val requestBody = request.body
+
+        val curlCommand = StringBuilder()
+        curlCommand.append("curl -X ${request.method} ")
+
+        // Append headers
+        for ((name, value) in request.headers) {
+            curlCommand.append("-H \"$name: $value\" ")
+        }
+
+        // Append request body
+        requestBody?.let {
+            val buffer = Buffer()
+            requestBody.writeTo(buffer)
+            val charset: Charset = requestBody.contentType()?.charset(UTF8) ?: UTF8
+            curlCommand.append("--data '").append(buffer.readString(charset)).append("' ")
+        }
+
+        // Append URL
+        curlCommand.append("\"${request.url}\"")
+
+        return chain.proceed(request)
+    }
+}
