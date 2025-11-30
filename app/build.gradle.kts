06695bf566f64f3b4a42a763e9165b179e756a76
seungma93 <seungma93@naver.com>
2025-11-30 18:10:26 +0900

    Chore: 스와이프 리프레시 디펜던시 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0") // Gson 컨버터
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0") // 통신
+
+    // Swipe (스와이프 리프레시)
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 }
