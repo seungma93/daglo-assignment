4c238cd83b08fe193f58f8056c62c5ddd551f983
seungma93 <seungma93@naver.com>
2025-12-04 07:38:44 +0900

    Refactor: Parcelable 변경

--- app/src/main/java/com/seungma/daglo/domain/list/entity/CharactersLoadEntity.kt
+++ app/src/main/java/com/seungma/daglo/domain/list/entity/CharactersLoadEntity.kt
@@ -1,6 +1,10 @@
 package com.seungma.daglo.domain.list.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CharactersLoadEntity (
     val characters: List<CharacterEntity>,
     val isLast: Boolean
-)
+): Parcelable
